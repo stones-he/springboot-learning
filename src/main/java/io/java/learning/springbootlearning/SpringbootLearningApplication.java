5c9a58adab637238f444dee95441ce501718fc27
Stones <stones_he@163.com>
2022-05-27 13:30:53 +0800

    init

---
+++ src/main/java/io/java/learning/springbootlearning/SpringbootLearningApplication.java
@@ -0,0 +1,13 @@
+package io.java.learning.springbootlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/java/learning/springbootlearning/SpringbootLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package io.java.learning.springbootlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
